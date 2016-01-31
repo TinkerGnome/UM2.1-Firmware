e96a6153cfb515d87a4ee50e7877312648bc204b
TinkerGnome <tinkergnome@gmx.de>
2016-01-31 17:49:26 +0100

    limit heated bed power for dual extruders

--- Marlin/build.sh
+++ Marlin/build.sh
@@ -8,7 +8,7 @@
 #############################
 
 ##Which version name are we appending to the final archive
-export BUILD_NAME=16.01.3
+export BUILD_NAME=16.01.4
 
 #############################
 # Actual build script
